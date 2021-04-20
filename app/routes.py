a42cc8284d255a2faedf028f35f3d5806f4c2a5c
mechanizespider <57589523+mechanizespider@users.noreply.github.com>
2021-04-21 04:16:21 +0700

    newpages

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,12 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/team')
+def team():
+    return render_template("team.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
+@app.route('/register')
+def register():
+    return render_template("register.html")
